758d60e018799e2dc06d3d1aa5e9eff33f172bd7
Håkan Thörngren <hth313@gmail.com>
2020-05-14 21:54:50 -0700

    Remove key sequence macros
    These are no longer part of OS4, they were moved to Boost a while
    back.

--- src/OS4.h
+++ src/OS4.h
@@ -207,24 +207,6 @@ Flag_IntervalTimer:  .equ  7           ; Set when we are using the interval time
 
 ;;; **********************************************************************
 ;;;
-;;; Key sequence parsing.
-;;;
-;;; **********************************************************************
-
-acceptAllValues: .equlab xargumentEntry
-
-// Flag number to permit EEX key, use ParseNumber_AllowEEX below in your code.
-#define Flag_ParseNumber_AllowEEX  0
-
-// Helper macros
-#define OffsetParseNumberFlag 4
-#define _ParseNumberMask(flag) (1 << (flag + OffsetParseNumberFlag))
-
-// Mask bit for permitting EEX key
-#define ParseNumber_AllowEEX  _ParseNumberMask(Flag_ParseNumber_AllowEEX)
-
-;;; **********************************************************************
-;;;
 ;;; Key table support.
 ;;;
 ;;; **********************************************************************
